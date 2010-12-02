86b40f2b1238c691c53c22a37b3772b67a0fe470
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-12-02 08:41:12 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-line@1970 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ testsrc/dk/dda/ddieditor/line/util/Wiki2Ddi3ScannerTest.java
@@ -0,0 +1,14 @@
+package dk.dda.ddieditor.line.util;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Wiki2Ddi3ScannerTest {
+	@Test
+	public void processLine() throws Exception {
+		Wiki2Ddi3Scanner scanner = new Wiki2Ddi3Scanner(new Ddi3Helper());
+		scanner.startScanning(new File("resources/easy.txt"));
+		scanner.getDdi3Helper().resultToString();
+	}
+}
