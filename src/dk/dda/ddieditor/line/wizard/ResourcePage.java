7dcb8497735afef180e3ba8deaf2b070c6ceece8
Jannik Vestergaard Jensen <jvj@dda.dk>
2011-03-18 08:06:26 +0000

    Added BusyIndicator
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor-line@2451 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- src/dk/dda/ddieditor/line/wizard/ResourcePage.java
+++ src/dk/dda/ddieditor/line/wizard/ResourcePage.java
@@ -11,8 +11,10 @@ import org.ddialliance.ddieditor.ui.editor.widgetutil.referenceselection.Referen
 import org.ddialliance.ddieditor.ui.view.Messages;
 import org.ddialliance.ddiftp.util.Translator;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.ui.PlatformUI;
 
 public class ResourcePage extends WizardPage {
 	public static final String PAGE_NAME = "Ref";
@@ -30,109 +32,141 @@ public class ResourcePage extends WizardPage {
 
 	@Override
 	public void createControl(Composite parent) {
-		Editor editor = new Editor();
-		Group group = editor.createGroup(parent,
+		// init
+		Group group = new Editor().createGroup(parent,
 				Translator.trans("line.wizard.refpage.group"));
 
-		// universe ref
-		List<LightXmlObjectType> uniRefList = new ArrayList<LightXmlObjectType>();
-		try {
-			uniRefList = DdiManager.getInstance()
-					.getUniversesLight(null, null, null, null)
-					.getLightXmlObjectList().getLightXmlObjectList();
-		} catch (Exception e) {
-			// TODO
-		}
-		uniRefSelectCombo = editor.createRefSelection(group,
-				Messages.getString("VariableEditor.label.universeref"),
-				Messages.getString("VariableEditor.label.universeref"),
-				ReferenceType.Factory.newInstance(), uniRefList, false);
-		uniRefSelectCombo.addSelectionListener(
-				Messages.getString("VariableEditor.label.universeref"), null);
-
-		// create universe scheme - comment out 20110208
-		// Button universeSchemeCreate = editor.createCheckBox(group, "",
-		// Translator.trans("line.wizard.createunislabel"));
-		// universeSchemeCreate.addSelectionListener(new SelectionListener() {
-		// @Override
-		// public void widgetSelected(SelectionEvent e) {
-		// createUniverseScheme = ((Button) e.getSource()).getSelection();
-		// }
-		//
-		// @Override
-		// public void widgetDefaultSelected(SelectionEvent e) {
-		// // do nothing
-		// }
-		// });
-
-		// concept ref
-		List<LightXmlObjectType> conceptRefList = new ArrayList<LightXmlObjectType>();
-		try {
-			conceptRefList = DdiManager.getInstance()
-					.getConceptsLight(null, null, null, null)
-					.getLightXmlObjectList().getLightXmlObjectList();
-		} catch (Exception e) {
-			// TODO
-		}
-		conRefSelectCombo = editor.createRefSelection(group,
-				Messages.getString("VariableEditor.label.conceptref"),
-				Messages.getString("VariableEditor.label.conceptref"),
-				ReferenceType.Factory.newInstance(), conceptRefList, false);
-		conRefSelectCombo.addSelectionListener(
-				Messages.getString("VariableEditor.label.conceptref"), null);
-
-		// create concept scheme - comment out 20110208
-		// Button conceptSchemeCreate = editor.createCheckBox(group, "",
-		// Translator.trans("line.wizard.createconslabel"));
-		// conceptSchemeCreate.addSelectionListener(new SelectionListener() {
-		// @Override
-		// public void widgetSelected(SelectionEvent e) {
-		// createConceptScheme = ((Button) e.getSource()).getSelection();
-		// }
-		//
-		// @Override
-		// public void widgetDefaultSelected(SelectionEvent e) {
-		// // do nothing
-		// }
-		// });
-
-		// question scheme ref
-		List<LightXmlObjectType> questionSchemeRefList = new ArrayList<LightXmlObjectType>();
-		try {
-			questionSchemeRefList = DdiManager.getInstance()
-					.getQuestionSchemesLight(null, null, null, null)
-					.getLightXmlObjectList().getLightXmlObjectList();
-		} catch (Exception e) {
-			// TODO
-		}
-		quesRefSelectCombo = editor.createRefSelection(group,
-				Translator.trans("line.wizard.refpage.ques"),
-				Translator.trans("line.wizard.refpage.ques"),
-				ReferenceType.Factory.newInstance(), questionSchemeRefList,
-				false);
-		quesRefSelectCombo.addSelectionListener(
-				Translator.trans("line.wizard.refpage.ques"), null);
-
-		// main seq ref
-		List<LightXmlObjectType> seqRefList = new ArrayList<LightXmlObjectType>();
-		try {
-			seqRefList = DdiManager.getInstance()
-					.getSequencesLight(null, null, null, null)
-					.getLightXmlObjectList().getLightXmlObjectList();
-		} catch (Exception e) {
-			// TODO
-			e.printStackTrace();
-		}
-		seqRefSelectCombo = editor.createRefSelection(group,
-				Translator.trans("line.wizard.refpage.mainseqref"),
-				Translator.trans("line.wizard.refpage.mainseqref"),
-				ReferenceType.Factory.newInstance(), seqRefList, false);
-
-		seqRefSelectCombo.addSelectionListener(
-				Translator.trans("line.wizard.refpage.mainseqref"), null);
+		// create
+		RefreshRunnable longJob = new RefreshRunnable(group);
+		BusyIndicator
+				.showWhile(PlatformUI.getWorkbench().getDisplay(), longJob);
 
 		// finalize
 		setControl(group);
 		setPageComplete(true);
+	}
+
+	/**
+	 * Runnable wrapping view refresh to enable RCP busy indicator
+	 */
+	class RefreshRunnable implements Runnable {
+		Group group;
+		Editor editor = new Editor();
+
+		RefreshRunnable(Group group) {
+			this.group = group;
+		}
+
+		@Override
+		public void run() {
+			try {
+				// universe ref
+				List<LightXmlObjectType> uniRefList = new ArrayList<LightXmlObjectType>();
+				try {
+					uniRefList = DdiManager.getInstance()
+							.getUniversesLight(null, null, null, null)
+							.getLightXmlObjectList().getLightXmlObjectList();
+				} catch (Exception e) {
+					// TODO
+				}
+				uniRefSelectCombo = editor.createRefSelection(group,
+						Messages.getString("VariableEditor.label.universeref"),
+						Messages.getString("VariableEditor.label.universeref"),
+						ReferenceType.Factory.newInstance(), uniRefList, false);
+				uniRefSelectCombo.addSelectionListener(
+						Messages.getString("VariableEditor.label.universeref"),
+						null);
+
+				// create universe scheme - comment out 20110208
+				// Button universeSchemeCreate = editor.createCheckBox(group,
+				// "",
+				// Translator.trans("line.wizard.createunislabel"));
+				// universeSchemeCreate.addSelectionListener(new
+				// SelectionListener() {
+				// @Override
+				// public void widgetSelected(SelectionEvent e) {
+				// createUniverseScheme = ((Button)
+				// e.getSource()).getSelection();
+				// }
+				//
+				// @Override
+				// public void widgetDefaultSelected(SelectionEvent e) {
+				// // do nothing
+				// }
+				// });
+
+				// concept ref
+				List<LightXmlObjectType> conceptRefList = new ArrayList<LightXmlObjectType>();
+				try {
+					conceptRefList = DdiManager.getInstance()
+							.getConceptsLight(null, null, null, null)
+							.getLightXmlObjectList().getLightXmlObjectList();
+				} catch (Exception e) {
+					editor.showError(e);
+				}
+				conRefSelectCombo = editor.createRefSelection(group,
+						Messages.getString("VariableEditor.label.conceptref"),
+						Messages.getString("VariableEditor.label.conceptref"),
+						ReferenceType.Factory.newInstance(), conceptRefList,
+						false);
+				conRefSelectCombo.addSelectionListener(
+						Messages.getString("VariableEditor.label.conceptref"),
+						null);
+
+				// create concept scheme - comment out 20110208
+				// Button conceptSchemeCreate = editor.createCheckBox(group, "",
+				// Translator.trans("line.wizard.createconslabel"));
+				// conceptSchemeCreate.addSelectionListener(new
+				// SelectionListener() {
+				// @Override
+				// public void widgetSelected(SelectionEvent e) {
+				// createConceptScheme = ((Button)
+				// e.getSource()).getSelection();
+				// }
+				//
+				// @Override
+				// public void widgetDefaultSelected(SelectionEvent e) {
+				// // do nothing
+				// }
+				// });
+
+				// question scheme ref
+				List<LightXmlObjectType> questionSchemeRefList = new ArrayList<LightXmlObjectType>();
+				try {
+					questionSchemeRefList = DdiManager.getInstance()
+							.getQuestionSchemesLight(null, null, null, null)
+							.getLightXmlObjectList().getLightXmlObjectList();
+				} catch (Exception e) {
+					editor.showError(e);
+				}
+				quesRefSelectCombo = editor.createRefSelection(group,
+						Translator.trans("line.wizard.refpage.ques"),
+						Translator.trans("line.wizard.refpage.ques"),
+						ReferenceType.Factory.newInstance(),
+						questionSchemeRefList, false);
+				quesRefSelectCombo.addSelectionListener(
+						Translator.trans("line.wizard.refpage.ques"), null);
+
+				// main seq ref
+				List<LightXmlObjectType> seqRefList = new ArrayList<LightXmlObjectType>();
+				try {
+					seqRefList = DdiManager.getInstance()
+							.getSequencesLight(null, null, null, null)
+							.getLightXmlObjectList().getLightXmlObjectList();
+				} catch (Exception e) {
+					editor.showError(e);
+				}
+				seqRefSelectCombo = editor.createRefSelection(group,
+						Translator.trans("line.wizard.refpage.mainseqref"),
+						Translator.trans("line.wizard.refpage.mainseqref"),
+						ReferenceType.Factory.newInstance(), seqRefList, false);
+
+				seqRefSelectCombo.addSelectionListener(
+						Translator.trans("line.wizard.refpage.mainseqref"),
+						null);
+			} catch (Exception e) {
+				Editor.showError(e, this.getClass().getName());
+			}
+		}
 	}
 }
